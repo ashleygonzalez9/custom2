7049db226360d9b92ae1cc7bece1012506b74944
ashleygonzalez9 <37843561+ashleygonzalez9@users.noreply.github.com>
2020-01-24 15:45:05 -0800

    Add files via upload

---
+++ custom.rb
@@ -0,0 +1,129 @@
+# Welcome to Sonic Pi v3.1
+use_bpm 125
+
+
+live_loop :melody do
+  
+  
+  2.times  do
+    play :a2
+    sleep 0.5
+    play :e3
+    sleep 0.5
+    play :b3
+    sleep 0.45
+    play :e3
+    sleep 0.5
+    play :c4
+    sleep 0.5
+    play :b3
+    sleep 0.5
+    play :e3
+    sleep 0.5
+    play :b3
+    sleep 0.5
+  end
+  
+  sample :bd_klub, amp:5
+  
+  
+  2.times do
+    play :f2
+    sleep 0.5
+    play :cs3
+    sleep 0.5
+    play :gs3
+    sleep 0.5
+    play :cs3
+    sleep 0.5
+    play :a3
+    sleep 0.5
+    play :gs3
+    sleep 0.5
+    play :cs3
+    sleep 0.5
+    play :gs3
+    sleep 0.5
+  end
+  
+  sample :bd_klub, amp:5
+  
+  
+  1.times do
+    play :d3
+    sleep 0.5
+    play :a3
+    sleep 0.5
+    play :b4
+    sleep 0.5
+    play :a3
+    sleep 0.5
+    play :a4
+    sleep 0.5
+    play :a3
+    sleep 0.5
+    play :d3
+    sleep 0.5
+    play :a3
+    sleep 0.5
+  end
+  
+  sample :bd_klub, amp:5
+  
+  
+  1.times do
+    play :e3
+    sleep 0.5
+    play :b3
+    sleep 0.5
+    play :c5
+    sleep 0.5
+    play :b3
+    sleep 0.5
+    play :b4
+    sleep 0.5
+    play :b3
+    sleep 0.5
+    play :e3
+    sleep 0.5
+    play :b3
+    sleep 0.5
+  end
+  
+  sample :bd_klub, amp:5
+  
+  
+  1.times do
+    play :a2
+    sleep 0.5
+    play :e3
+    sleep 0.5
+    play :b4
+    sleep 0.5
+    play :e3
+    sleep 0.5
+    play :c4
+    sleep 0.5
+    play :b3
+    sleep 0.5
+    play :e3
+    sleep 0.5
+    play :b4
+    sleep 0.5
+    
+  end
+  
+  
+  
+end
+
+
+
+
+
+
+
+
+
+
+
